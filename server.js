8aa06de7ba5f7a8991a13981d4eea51cdc15edb2
EvgeniiEfim <119408009+EvgeniiEfim@users.noreply.github.com>
2024-11-02 20:49:59 +0300

    Добавили стили

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
